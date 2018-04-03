5b2b84fbcc808af53cc26c96246c6c9788c7ca8a
qvil <qvil1127@gmail.com>
2018-04-03 23:56:53 +0900

    redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { reducer } from "./reducer";
 
 import createSagaMiddleware from "redux-saga";
 import { helloSaga } from "./sagas";
 
+const composeEnhancers =
+  process.env.NODE_ENV !== "production"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 const action = type => store.dispatch({ type });
 
 sagaMiddleware.run(helloSaga);
